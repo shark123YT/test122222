745186420c7f8bd8ccd750cd9831392a9fce5358
shark123YT <137716913+shark123YT@users.noreply.github.com>
2023-06-25 19:11:50 +0200

    Add files via upload

---
+++ TESTmod.js
@@ -0,0 +1,9 @@
+// Adding elements:
+elements.TEST1 = {
+    color: "#133738",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 100000,
+    state: "liquid",
+    density: 720,
+};
